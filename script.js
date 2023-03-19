e0546a916e0b4e16c5f5bb3d07c5793eb1dceab6
SoraI0 <romamogir@gmail.com>
2023-03-20 00:01:43 +0200

    [task 3]

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-function correctnes(a) {
+function correctnes(a, ifNuN) {
 	let isTrue = false;
-	while (!isTrue) {
-		if (a !== null && a.length > 0 && a.length <= 50) {
-			isTrue = true;
-		} else {
-			a = window.prompt('Помилка. Уведіть ще раз:', '');
+	if (ifNuN) {
+		while (!isTrue) {
+			if (a !== null && a.length > 0 && a.length <= 50) {
+				isTrue = true;
+			} else {
+				a = window.prompt('Помилка. Уведіть ще раз:', '');
+			}
+		}
+	} else {
+		while (!isTrue) {
+			if (a !== null && a !== '' && !isNaN(a)) {
+				isTrue = true;
+			} else {
+				a = window.prompt('Помилка. Уведіть ще раз:', '');
+			}
 		}
 	}
 	return a;
 }
 
-let numberOfFilms = correctnes(window.prompt('Скільки фільмів ви вже переглянули?', ''));
+function detectPersonalLevel() {
+	if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+		console.log('Переглянуто доволі мало фільмів');
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count === 30) {
+		console.log('Ви дефолтний глядач');
+	} else if (personalMovieDB.count > 30) {
+		console.log('Ви кіноман');
+	} else {
+		console.log('Відбулась помилка');
+	}
+}
+
+function showMyDB(isPrivat, movies) {
+	if (isPrivat) {
+		return;
+	}
+	console.log(movies);
+}
+
+function writeMyGenres() {
+	let genres = [];
+	for (let i = 0; i < 3; i++) {
+		genres[i] = window.prompt(`Ваш улюблений жанр під номером ${i + 1}`);
+	}
+	return genres;
+}
+
+function rememberMyFilm() {
+	for (let i = 0; i < personalMovieDB.count; i++) {
+		let film, score;
+		film = correctnes(window.prompt(`${i + 1} Уведіть назву фільму:`, ''), true);
+		score = correctnes(window.prompt(`${i + 1} Уведіть оцінку:`, '', false));
+		personalMovieDB.movies[film] = +score;
+	}
+}
+
+let numberOfFilms = correctnes(window.prompt('Скільки фільмів ви вже переглянули?', ''), false);
 
 const personalMovieDB = {
 	count: +numberOfFilms,
@@ -20,23 +66,14 @@ const personalMovieDB = {
 	privat: false
 };
 
-for (i = 0; i < personalMovieDB.count; i++) {
-	let film, score;
-	film = correctnes(window.prompt(`${i + 1} Уведіть назву фільму:`, ''));
-	score = correctnes(window.prompt(`${i + 1} Уведіть оцінку:`, ''));
-	personalMovieDB.movies[film] = +score;
-}
+rememberMyFilm()
+detectPersonalLevel();
+showMyDB(personalMovieDB.privat, personalMovieDB);
+personalMovieDB.genres = writeMyGenres();
 
-console.log(personalMovieDB.movies);
-if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
-	console.log('Переглянуто доволі мало фільмів');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count === 30) {
-	console.log('Ви дефолтний глядач');
-} else if (personalMovieDB.count > 30) {
-	console.log('Ви кіноман');
-} else {
-	console.log('Відбулась помилка');
-}
+
+
+
 
 
 
